fea2040bd6cb75340bfdc9118d5f27bc31f38d01
vhforbes <vhforbes@gmail.com>
2025-05-27 16:59:08 -0300

    migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250527_195838 from './20250527_195838';
+
+export const migrations = [
+  {
+    up: migration_20250527_195838.up,
+    down: migration_20250527_195838.down,
+    name: '20250527_195838'
+  },
+];
